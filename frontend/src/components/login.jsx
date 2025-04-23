e8dd95e41d2df0f222363b90b4b795ac416cbe35
Ifer506 <shakyarajsumedha@gmail.com>
2025-04-23 12:39:30 +0545

    login design start

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import React from "react";
-import logo from "../assets/till_date2.png"
+import logo from "../assets/till_date2.png";
 // import { useAuth } from "../utils/authContext";
 
 const LoginPage = () => {
-  //   const [email, setEmail] = useState("");
-  //   const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
   //   const [showPassword, setShowPassword] = useState(false);
   //   const toggleShowPassword = () => {
   //     setShowPassword(!showPassword);
@@ -17,8 +18,10 @@ const LoginPage = () => {
   //   // const dispatch = useDispatch()
   //   const auth = useAuth();
 
-  //   const handleSubmit = async (e) => {
-  //     e.preventDefault();
+    const handleSubmit = async (e) => {
+      concole.log(setEmail,setPassword)
+    };
+      // e.preventDefault();
 
   //     try {
   //       if (!email || !password) {
@@ -48,16 +51,18 @@ const LoginPage = () => {
   //   };
 
   return (
-    <div class="bg-white dark:bg-gray-900">
-      <div class="flex justify-center h-screen ">
-        <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6  ">
+    <div class=" bg-gradient-to-br from-teal-950 via-teal-700 to-slate-900
+">
+      <div class="h-screen flex items-center justify-center ">
+        <div class="flex items-center w-full max-w-md  px-6 mx-auto lg:w-2/6 p-8 rounded-2xl ring ring-teal-900 shadow-lg/30 shadow-black-500/30 ">
           <div class="flex-1">
             <div class="text-center">
               <div class="flex justify-center mx-auto ">
                 <img
-                  class="w-auto h-10 sm:h-8"
+                  class="w-auto h-10 sm:h-30"
                   // src="./assets/till_date2.png"
-                  img src={logo}
+                  img
+                  src={logo}
                   alt="till_data logo"
                 />
               </div>
@@ -80,6 +85,8 @@ const LoginPage = () => {
                     type="email"
                     name="email"
                     id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="example@example.com"
                     class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
@@ -106,13 +113,18 @@ const LoginPage = () => {
                     name="password"
                     id="password"
                     placeholder="Your Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                   />
                 </div>
 
                 <div class="mt-6">
-                  <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                  <button type = "submit"
+                    onClick = {handleSubmit}
+                    class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                     Sign in
+                    
                   </button>
                 </div>
               </form>
