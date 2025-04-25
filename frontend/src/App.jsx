c7c73fa312337649387b4978ee3244a5cb44e68b
Ifer506 <shakyarajsumedha@gmail.com>
2025-04-25 15:56:30 +0545

    Removal of one line fixed navigating to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import { Toaster, toast } from 'react-hot-toast';
-import './App.css'
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import Home from "./components/home";
-import  LoginPage  from './components/login';
+import LoginPage from "./components/login";
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      
-    <LoginPage/>
-    <Routes>
-    <Route path="/" element={<LoginPage />} />
-    <Route path="/home" element={<Home />} />
-
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
